9e0ab02295f5f099c79c7b452c04e44346bef92b
牵猫散步的鱼 <1727708837@qq.com>
2021-11-15 10:51:55 +0800

    设计DDD驱动

---
+++ Moudles/Sigle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moudles
+{
+    public class SingleNew
+    {
+        /// <summary>
+        /// 控制对象的实例化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T GetT<T>() where T : class,new ()
+        {
+            return new T();
+        }
+    }
+}
---
+++ SimWebFrame/Controllers/SimApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimWebFrame.Controllers
+{
+    public class SimApiController : ApiController
+    {
+        // GET: api/SimApi
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/SimApi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/SimApi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/SimApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/SimApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
